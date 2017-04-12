e67b61bb3c8a4540704c51c8dd3aaeaa51bb4316
Reed Nelson-Saunders <reedns@hotmail.com>
2017-04-12 10:19:47 -0700

    get events from the api and display them

--- app/assets/javascripts/components/event_table.js.jsx
+++ app/assets/javascripts/components/event_table.js.jsx
@@ -1,5 +1,9 @@
 var EventTable = React.createClass({
   render: function() {
+    var events = [];
+    this.props.events.forEach(function(e) {
+      events.push(<Event event={e} key={'event' + e.id}/>);
+    }.bind(this));
     return(
       <table className='table table-striped'>
         <thead>
@@ -10,6 +14,7 @@ var EventTable = React.createClass({
             <th className='col-md-4'>Description</th>
           </tr>
         </thead>
+        <tbody>{events}</tbody>
       </table>
     )
   }
